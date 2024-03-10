22dce03d9e9606b661eabfaaaff7c6c04535f8a7
Vibur PLP <vickarish@gmail.com>
2024-03-10 14:18:27 +0300

    Data Types in Dart Technical Assignment

---
+++ css/Dart_Data_Types_Ass.dart
@@ -0,0 +1,18 @@
+void main() {
+// Integer data type
+  int Age = (30);
+  print('I am $Age old');
+// Double data type
+  double birthday = 1991.01;
+  print("I was born in $birthday");
+
+// String data type
+  String name = "Dart";
+  print('My name is $name');
+// List data type
+  List<String> FavoriteFoods = ['Yam', 'Potatoes', 'arrow roots'];
+  print('my favorite foods include $FavoriteFoods among many');
+// Map data type
+  Map<int, String> particulars = {38: 'ShoeSize', 5: 'Friends', 32: 'teeth'};
+  print('My particulars are: $particulars');
+}
